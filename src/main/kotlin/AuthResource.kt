acca9632822604b535f1ed2673fc80e915c8afd6
sara-jbr <sarajabbari@mobilityonegroup.com>
2025-01-31 15:45:54 +0800

    feat: Add login api

---
+++ src/main/kotlin/AuthResource.kt
@@ -0,0 +1,22 @@
+import io.quarkus.security.Authenticated
+import jakarta.inject.Inject
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.core.Response
+import org.eclipse.microprofile.jwt.JsonWebToken
+
+
+@Path("/login")
+class LoginResource {
+
+    @Inject
+    lateinit var jwt: JsonWebToken
+
+    @GET
+    @Authenticated
+    fun login(): Response {
+        // This is a simple example. You can also include user data or token info.
+        val username = jwt.name
+        return Response.ok("Hello, $username!").build()
+    }
+}
